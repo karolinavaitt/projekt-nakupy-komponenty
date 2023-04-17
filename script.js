0f022f079fc954d319c4a8ca8173107d0c1a5547
Martin Podloucký <podlouckymartin@gmail.com>
2023-04-17 17:38:48 +0200

    Rozdělení na komponenty

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('funguju!');
+import { ShopList } from "./ShopList/index.js";
+
+const mainElement = document.querySelector('main');
+
+fetch('https://nakupy.kodim.app/api/sampleweek/mon/items')
+  .then((response) => response.json())
+  .then((data) => {
+    mainElement.innerHTML += ShopList(
+      { dayName: 'Pondělí', items: data.result }
+    );
+  });
+
+fetch('https://nakupy.kodim.app/api/sampleweek/tue/items')
+  .then((response) => response.json())
+  .then((data) => {
+    mainElement.innerHTML += ShopList(
+      { dayName: 'Úterý', items: data.result }
+    );
+  });
